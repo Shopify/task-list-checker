497b6354b4603e78bd9c9d17828616fce27199ca
Ash Furrow <ash.furrow@shopify.com>
2021-06-18 10:00:59 -0300

    Only runs on PR events.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ try {
   if (!GITHUB_SECRET) {
     throw "Missing GITHUB_SECRET input";
   }
-  // TODO: Only check for PRs
+  if (!['pull_request', 'pull_request_review', 'pull_request_review_comment'].includes(github.context.eventName)) {
+    throw `Being invoked for non-PR event "${github.context.eventName}"`;
+  }
   const prDescription = github.context.payload.pull_request.body;
   console.log(prDescription);
 } catch (error) {
