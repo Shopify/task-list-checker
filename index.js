1c4acbd9446fb61c52e4b4a36c63c9ef820032ee
JM Neri <jm.neri@shopify.com>
2021-07-20 15:09:39 -0600

    Testing walkTokens

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const marked = require('marked');
 
+function nestedChecklistItems(tokens) {
+  /** @type Array<{checked: boolean; ourText: string}> */
+  let checklistItems = []
+  function addNestedChecklistItems(tokens) {
+    tokens.forEach(token => {
+      if (token.type === 'list') {
+        addNestedChecklistItems(token.items)
+      } else {
+        if (token.checked === true || token.checked === false) {
+          const ourText = token.tokens
+            .filter(token => token.type !== 'list')
+            .map(token => token.text).join()
+          checklistItems = checklistItems.concat({checked: token.checked, ourText})
+        }
+        addNestedChecklistItems(token.tokens)
+      }
+    })
+  }
+  addNestedChecklistItems(tokens)
+  return checklistItems
+}
+
 // This function taken from https://github.com/stilliard/github-task-list-completed/blob/master/src/check-outstanding-tasks.js
 function checkOutstandingTasks(body, skipTokens) {
   if (body === null) {
@@ -15,24 +37,27 @@ function checkOutstandingTasks(body, skipTokens) {
   }
   
   const tokens = marked.lexer(body, { gfm: true });
-  /** @type marked.Tokens.ListItem[] */
-  const listItems = tokens.reduce((acc, token) => {
-    if (token.type !== 'list') {
-      return acc
+  let checklistItems = []
+  marked.walkTokens(tokens, token => {
+    if (token.type !== 'list_item') return
+    if (token.checked === true || token.checked === false) {
+      const ourText = token['tokens']
+        .filter(token => token.type !== 'list')
+        .map(token => token.text).join()
+      checklistItems = checklistItems.concat({checked: token.checked, ourText})
     }
-    return [...acc, ...token.items]
-  }, [])
+  })
 
-  console.log(JSON.stringify({listItems}))
-  const prunedItems = listItems.filter(item => skipTokens.filter(
-    token => item.text.includes(token)
+  console.log(JSON.stringify({checklistItems}))
+  const prunedItems = checklistItems.filter(item => skipTokens.filter(
+    token => item.ourText.includes(token)
   ).length == 0)
 
   // return counts of task list items and how many are left to be completed
   return {
-    total: prunedItems.filter(item => item.checked !== undefined).length,
+    total: prunedItems.length,
     remaining: prunedItems.filter(item => item.checked === false).length,
-    skipped: (listItems.length - prunedItems.length)
+    skipped: (checklistItems.length - prunedItems.length)
   };
 };
 
