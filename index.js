80446bc2cae5487a3507193684699a0bf1ce0a6b
JM Neri <jm.neri@shopify.com>
2021-07-20 08:26:28 -0600

    Updated status description

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function checkOutstandingTasks(body) {
 
 async function run() {
   try {
-    const startTime = (new Date).toISOString();
     const GITHUB_TOKEN = core.getInput('github-token');
     if (!GITHUB_TOKEN) {
       throw "Missing github-token input";
@@ -48,43 +47,17 @@ async function run() {
     const prDescription = pr.body;
     const outstandingTasks = checkOutstandingTasks(prDescription);
 
-    // let check = {
-    //   name: 'check-for-incomplete-tasks',
-    //   head_sha: pr.head.sha,
-    //   started_at: startTime,
-    //   conclusion: 'failure',
-    //   output: {
-    //     title: (outstandingTasks.total - outstandingTasks.remaining) + ' / ' + outstandingTasks.total + ' tasks completed',
-    //     summary: outstandingTasks.remaining + ' task' + (outstandingTasks.remaining > 1 ? 's' : '') + ' still to be completed',
-    //     text: 'We check if any task lists need completing before you can merge this PR'
-    //   }
-    // }
-    
-    // // all finished?
-    // if (outstandingTasks.remaining === 0) {
-    //   check.conclusion = 'success';
-    //   check.completed_at = (new Date).toISOString();
-    //   check.output.summary = 'All tasks have been completed';
-    // };
-
     console.log({prDescription, outstandingTasks})
 
-    // TODO: experimenting with creating a commit status directly
-    // TODO: add target_url?
+    // TODO: include more help in the description
     await octokit.rest.repos.createCommitStatus({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       sha: pr.head.sha,
       state: (outstandingTasks.remaining > 0) ? 'pending' : 'success',
-      description: (outstandingTasks.total - outstandingTasks.remaining) + '/' + outstandingTasks.total + ' [x] completed',
+      description: (outstandingTasks.total - outstandingTasks.remaining) + '/' + outstandingTasks.total + ' checklist items in the PR description have been completed',
       context: 'task-list-checker',
     })
-    // send check back to GitHub
-    // await octokit.rest.checks.create({...github.context.repo, ...check});
-
-    // if (outstandingTasks.remaining > 0) {
-    //   core.setFailed(`Testing core.setFailed output: ${check.output.summary}`)
-    // }
   } catch (error) {
     core.setFailed(error.message);
   }
