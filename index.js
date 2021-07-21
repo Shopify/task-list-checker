d4037f7c22aed4097ff18c9ea2341dca6e9c9836
JM Neri <jm.neri@shopify.com>
2021-07-21 08:20:53 -0600

    Adding more type annotations

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // @ts-check
 
-const core = require('@actions/core');
-const github = require('@actions/github');
-const marked = require('marked');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const marked = require('marked')
 
+/**
+ * @param {string} text 
+ * @returns {string}
+ */
 function textWithoutSublists(text) {
   return text.split('\n')[0]
 }
 
+/**
+ * @param {string} body 
+ * @returns {{checked: boolean; ourText: string}[]}
+ */
 function extractChecklistItems(body) {
-  const tokens = marked.lexer(body, { gfm: true });
+  const tokens = marked.lexer(body, { gfm: true })
+
+  /** @type {{checked: boolean; ourText: string}[]} */
   let checklistItems = []
   marked.walkTokens(tokens, token => {
     if (token.type !== 'list_item') return
@@ -22,13 +32,18 @@ function extractChecklistItems(body) {
 }
 
 // This function taken from https://github.com/stilliard/github-task-list-completed/blob/master/src/check-outstanding-tasks.js
+/**
+ * @param {string | undefined} body 
+ * @param {string[]} skipTokens 
+ * @returns {{total: number; remaining: number; skipped: number}}
+ */
 function checkOutstandingTasks(body, skipTokens) {
-  if (body === null) {
+  if (body === undefined) {
       return {
           total: 0,
           remaining: 0,
           skipped: 0
-      };
+      }
   }
 
   const checklistItems = extractChecklistItems(body)
@@ -38,30 +53,29 @@ function checkOutstandingTasks(body, skipTokens) {
 
   console.log({checklistItems, prunedItems})
 
-  // return counts of task list items and how many are left to be completed
   return {
     total: prunedItems.length,
     remaining: prunedItems.filter(item => item.checked === false).length,
     skipped: (checklistItems.length - prunedItems.length)
-  };
-};
+  }
+}
 
 async function run() {
   try {
-    const GITHUB_TOKEN = core.getInput('github-token');
+    const GITHUB_TOKEN = core.getInput('github-token')
     if (!GITHUB_TOKEN) {
-      throw "Missing github-token input";
+      throw "Missing github-token input"
     }
     if (!['pull_request'].includes(github.context.eventName)) {
-      throw `Being invoked for an event that does not change the PR description "${github.context.eventName}"`;
+      throw `Being invoked for an event that does not change the PR description "${github.context.eventName}"`
     }
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+    const octokit = github.getOctokit(GITHUB_TOKEN)
     const pr = github.context.payload.pull_request
 
-    const prDescription = pr.body;
+    const prDescription = pr.body
     const skipTokens = ['POST-MERGE:', 'N/A']
-    const outstandingTasks = checkOutstandingTasks(prDescription, skipTokens);
+    const outstandingTasks = checkOutstandingTasks(prDescription, skipTokens)
 
     console.log({prDescription, outstandingTasks})
 
@@ -75,7 +89,7 @@ async function run() {
       context: '- [ ] checklist completion',
     })
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
