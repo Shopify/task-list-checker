1255059e928059349ee0a662974a7165c4c728e9
JM Neri <jm.neri@shopify.com>
2021-08-12 12:02:04 -0600

    Using default value to simplify the code

--- index.js
+++ index.js
@@ -5,37 +5,19 @@ const run = require('./utils/run')
 const tagging = require('./utils/tagging')
 
 /**
- * @param {'github-token' | 'readme-url'} name
- * @returns {string | undefined}
+ * @param {'github-token' | 'readme-url'} name 
+ * @returns {string}
  */
-function getOptionalInput(name) {
+function getInput(name) {
   const value = core.getInput(name)
-  if (value === '') return undefined
-  else return value
-}
-
-/**
- * @param {'github-token'} name 
- * @returns {string}
- */
-function getRequiredInput(name) {
-  const value = getOptionalInput(name)
-  if (value === undefined) throw `Missing "${name}" input`
+  if (value === '') throw `Missing "${name}" input`
   return value
-}
-
-/**
- * @returns {string}
- */
-function getReadmeURL() {
-  const custom = getOptionalInput('readme-url')
-  return custom ?? 'https://github.com/Shopify/task-list-checker#in-a-pull-request'
 }
 
 try {
   run.reportChecklistCompletion({
-    githubToken: getRequiredInput('github-token'),
-    readmeURL: getReadmeURL(),
+    githubToken: getInput('github-token'),
+    readmeURL: getInput('readme-url'),
     rule: tagging.rule({skip: ['POST-MERGE:', 'N/A']})
   })
 } catch (error) {
